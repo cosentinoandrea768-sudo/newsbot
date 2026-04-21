9387cf275646e5d7075548101603ebb76c0406ec
Demon <cosentinoandrea768@gmail.com>
2026-04-21 11:54:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,6 @@ def translate_text(text):
 # ==============================
 RSS_FEEDS = [
     "https://www.investing.com/rss/news_14.rss",
-    "https://www.investing.com/rss/news_1.rss",
     "https://www.investing.com/rss/news_11.rss"
 ]
 
